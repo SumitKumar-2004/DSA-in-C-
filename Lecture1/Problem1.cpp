517e7b774531b5ff06cc4e21c10bbaa2e0fe7c21
SUMIT KUMAR <125842926+SumitKumar-2004@users.noreply.github.com>
2025-11-28 10:12:27 +0530

    Update Problem1.cpp

--- Lecture1/Problem1.cpp
+++ Lecture1/Problem1.cpp
@@ -1,2 +1,24 @@
 //Calculate "Simple Interest" from principle(p), rate (r) and time(t)
+#include <iostream>
+using namespace std;
+
+int main() {
+    float p, r, t, si;
+
+    cout << "Enter Principle Amount (P): ";
+    cin >> p;
+
+    cout << "Enter Rate of Interest (R): ";
+    cin >> r;
+
+    cout << "Enter Time (T in years): ";
+    cin >> t;
+
+    si = (p * r * t) / 100;
+
+    cout << "Simple Interest = " << si << endl;
+
+    return 0;
+}
+
 
