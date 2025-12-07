743d6949d1076d49ad3547fc8114fc63d415f782
SUMIT KUMAR <125842926+SumitKumar-2004@users.noreply.github.com>
2025-12-07 17:40:15 +0530

    Add files via upload

---
+++ Lecture22/PaintersPartitionProblem.cpp
@@ -0,0 +1,59 @@
+// Painter Partition Problem
+// Time complexity : (log(sum) * n)
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+bool isPossible(vector<int> &arr, int n, int m, int maxAllowedTime) // O(n)
+{
+   int painters = 1, time = 0;
+   for (int i = 0; i < n; i++)
+   {
+      if (time + arr[i] <= maxAllowedTime)
+      {
+         time += arr[i];
+      }
+      else
+      {
+         painters++;
+         time = arr[i];
+      }
+   }
+   return painters <= m;
+}
+
+int minTimeToPaint(vector<int> &arr, int n, int m) //
+{
+   int sum = 0, maxVal = INT_MIN;
+   for (int i = 0; i < n; i++)
+   {
+      sum += arr[i];
+      maxVal = max(maxVal, arr[i]);
+   }
+   int st = maxVal, end = sum, ans = -1;
+   while (st <= end) //(log(sum) * n) or log(range)
+   {
+      int mid = st + (end - st) / 2;
+      if (isPossible(arr, n, m, mid))
+      {
+         ans = mid;
+         end = mid - 1;
+      }
+      else
+      {
+         st = mid + 1;
+      }
+   }
+   return ans;
+}
+
+int main()
+{
+   vector<int> arr = {40, 30, 10, 20};
+   // here n -> no. of array elements and
+   // m -> no. of painters
+   int n = 4, m = 2;
+   cout << minTimeToPaint(arr, n, m) << endl;
+   return 0;
+}
